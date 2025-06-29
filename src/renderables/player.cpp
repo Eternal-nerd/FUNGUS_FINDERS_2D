d7eeb194f298f0c7d3d048be8b2d36459f20f8b9
Myles Master <myles1551@gmail.com>
2025-06-29 11:16:46 -0500

    remove duplicate code

--- src/renderables/player.cpp
+++ src/renderables/player.cpp
@@ -178,32 +178,27 @@ void Player::update() {
 }
 
 void Player::updateTextureCoords() {
-    // TODO relocate textureMirrored_ updates to here (from onKey)
+    textureMirrored_ = false;
+
     switch (movementState_) {
     case FORWARD:
         texturePosition_ = {0.f, 0.25f};
-        textureMirrored_ = false;
-        break;
-    case FORWARD_RIGHT:
-        texturePosition_ = {0.75f, 0.f};
-        textureMirrored_ = false;
         break;
     case FORWARD_LEFT:
-        texturePosition_ = {0.75f, 0.f};
         textureMirrored_ = true;
+    case FORWARD_RIGHT:
+        texturePosition_ = {0.75f, 0.f};
         break;
     case DOWN:
         texturePosition_ = {0.f, 0.f};
-        textureMirrored_ = false;
-        break;
-    case DOWN_RIGHT:
-        texturePosition_ = {0.25f, 0.f};
-        textureMirrored_ = false;
         break;
     case DOWN_LEFT:
-        texturePosition_ = {0.25f, 0.f};
         textureMirrored_ = true;
+    case DOWN_RIGHT:
+        texturePosition_ = {0.25f, 0.f};
         break;
+    case LEFT:
+        textureMirrored_ = true;
     case RIGHT:
         switch (textureStage_) {
         case 0:
@@ -217,29 +212,13 @@ void Player::updateTextureCoords() {
             texturePosition_ = {0.5f, 0.25f};
             break;
         }
-        textureMirrored_ = false;
-        break;
-    case LEFT:
-        switch (textureStage_) {
-        case 0:
-        case 2:
-            texturePosition_ = {0.5f, 0.f};
-            break;
-        case 1:
-            texturePosition_ = {0.25f, 0.25f};
-            break;
-        case 3:
-            texturePosition_ = {0.5f, 0.25f};
-            break;
-        }
-        textureMirrored_ = true;
         break;
     case STOPPED:
         texturePosition_ = {0.f, 0.f};
-        textureMirrored_ = false;
         break; 
     }
-
+    
+    // TODO - fix this
     // update vertices
     if (textureMirrored_) {
 	    vertices_[0].texCoord = { texturePosition_.x + textureOffset_, texturePosition_.y };
